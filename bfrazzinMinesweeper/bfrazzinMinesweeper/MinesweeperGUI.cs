e2b4777dab2a868bb369877986344167bf31543b
bfrazzin <89413403+bfrazzin@users.noreply.github.com>
2023-01-22 21:13:08 -0700

    Add files via upload

---
+++ bfrazzinMinesweeper/bfrazzinMinesweeper/MinesweeperGUI.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace bfrazzinMinesweeper
+{
+    public partial class MinesweeperGUI : Form
+    {
+        Cell[,] cells = new Cell[10, 10];
+        public MinesweeperGUI()
+        {
+            InitializeComponent();
+            CreateGrid();
+        }
+
+        public void CreateGrid()
+        {
+            // NOT FIN
+            for(int row = 0; row < cells.GetLength(0); row++ )
+            {
+                for(int col = 0; col < cells.GetLength(1); col++)
+                {
+                    cells[row, col] = new Cell();
+                    cells[row, col].Row = row;
+                    cells[row, col].Col = col;
+                    cells[row, col].CellClick += CellClickHandler;
+                    
+                }
+            }
+        }
+
+        //handler for cell click
+        public void CellClickHandler(object sender, EventArgs e)
+        {
+            //click on red and it clicks on the blotch of red around it
+            Cell currentCell = ((Cell)sender);
+            // we have the color of the cell we just clicked
+            Color targetColor = currentCell.BackColor;
+
+
+            CheckAbove(currentCell, targetColor);
+            CheckBelow(currentCell, targetColor);
+
+
+            // do the help methods then delete this
+            // check above. these if statements check if they find a match
+            // first have to check if there is a row above it
+            // TRY TO CREATE THESE INTO HELPER METHODS. extract method auto featrure
+            // TO HELP UNDERSTAND WHAT IS HAPPENEING CELL BY CELL DO THREAD.SLEEP(500
+            NewMethod(currentCell, targetColor);
+
+            private void CheckBelow(Cell currentCell, Color targetColor)
+            {
+                if (currentCell.Row < cells.GetLength(0) - 1)
+                {
+                    // CHECK DIRECTLY ABOVE 
+                    if (targetColor == cells[currentCell.Row + 1, currentCell.Col].BackColor)
+                    {
+                        cells[currentCell.Row + 1, currentCell.Col].PerformClick();
+                    }
+                    // CHECK ABOVE ? AND TO THE LEFT
+                    if (currentCell.Col > 0)
+                    {
+                        if (targetColor == cells[currentCell.Row + 1, currentCell.Col - 1].BackColor)
+                        {
+                            cells[currentCell.Row + 1, currentCell.Col - 1].PerformClick();
+                        }
+                    }
+                    // CHECK RIGHT?
+                    if (currentCell.Col < cells.GetLength(1) - 1)
+                    {
+                        if (targetColor == cells[currentCell.Row + 1, currentCell.Col + 1].BackColor)
+                        {
+                            cells[currentCell.Row + 1, currentCell.Col + 1].PerformClick();
+                        }
+
+                    }
+                }
+            }
+
+            private void CheckSide(Cell currentCell, Color targetColor)
+
+
+        }
+
+        private void NewMethod(Cell currentCell, Color targetColor)
+        {
+            if (currentCell.Row > 0)
+            {
+                // CHECK DIRECTLY ABOVE 
+                if (targetColor == cells[currentCell.Row - 1, currentCell.Col].BackColor)
+                {
+                    cells[currentCell.Row - 1, currentCell.Col].PerformClick();
+                }
+                // CHECK ABOVE ? AND TO THE LEFT
+                if (currentCell.Col > 0)
+                {
+                    if (targetColor == cells[currentCell.Row - 1, currentCell.Col - 1].BackColor)
+                    {
+                        cells[currentCell.Row - 1, currentCell.Col - 1].PerformClick();
+                    }
+                }
+                // CHECK RIGHT?
+                if (currentCell.Col < cells.GetLength(1) - 1)
+                {
+                    if (targetColor == cells[currentCell.Row - 1, currentCell.Col + 1].BackColor)
+                    {
+                        cells[currentCell.Row - 1, currentCell.Col + 1].PerformClick();
+                    }
+
+                }
+            }
+        }
+    }
+}
+if (targetColor == cells[currentCell.Row - 1, currentCell.Col].BackColor)
+{
+    cells[currentCell.Row - 1, currentCell.Col - 1].PerformClick();
+}
+if (currentCell.Col > 0)
+{
+    if (targetColor == cells[currentCell.Row - 1, currentCell.Col - 1].BackColor)
+    {
+        cells[currentCell.Row - 1, currentCell.Col - 1].PerformClick();
+    }
+    if (currentCell.Col < cells.GetLength(1))
+    {
+        // CHECK TO THE RIGHT
+        if (targetColor == cells[currentCell.Row - 1, currentCell.Col + 1].BackColor)
+        {
+            cells[currentCell.Row - 1, currentCell.Col - 1].PerformClick();
+        }
+    }
+}
---
+++ bfrazzinMinesweeper/bfrazzinMinesweeper/MinesweeperLogic.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bfrazzinMinesweeper
+{
+    public class MinesweeperLogic
+    {
+        Random rand = new Random();
+        public void PlaceBombsHandler(object sender, PlaceBombEventArgs e)
+        {
+            // determined where the user clicked
+            // CCLEAN THIS WHOLE PIECE OF SHIT UP USING HELPER METHODS!!!!!!!!!!!!!!!!!!!!!!!!!!!
+            int clickedRow = e.ClickLocation.Item1;
+            int clickedCol = e.ClickLocation.Item2;
+
+            // now generate the bombs
+            List<Tuple<int, int>> response = new List<Tuple<int, int>>();
+
+            // 10 bombs
+            for(int i = 0; i < 10; i++)
+            {
+                bool validChoice = false;
+                do
+                {
+                    // place within the grid
+                    // CONSIDER CHANGING THE 10 VALUE TO A VARIABLE
+                    Tuple<int, int> bombLocation = Tuple.Create(rand.Next(10), rand.Next(10));
+
+                    // check to see if it is a valid choice
+
+                    // confirm that the bomb is not where we have clicked
+                    validChoice = CheckIfValidLocation(clickedRow, clickedCol, response, validChoice, bombLocation);
+                } while (!validChoice);
+                
+            }
+
+            // use hasBomb boolean?
+        }
+
+        private static bool CheckIfValidLocation(int clickedRow, int clickedCol, List<Tuple<int, int>> response, bool validChoice, Tuple<int, int> bombLocation)
+        {
+            if (bombLocation.Item1 != clickedRow && bombLocation.Item2 != clickedCol)
+            {
+                // check all of the locations
+                validChoice = true;
+                foreach (var location in response)
+                {
+
+                    if (bombLocation.Item1 == location.Item1 && bombLocation.Item2 == location.Item2)
+                    {
+                        validChoice = false;
+                    }
+                }
+            }
+
+            return validChoice;
+        }
+    }
+}
